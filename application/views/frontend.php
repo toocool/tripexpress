08d9ec535ca432f8e8db4116ffc619d182c16b4b
Shpetim /PC <info@arbamedia.com>
2014-08-22 13:54:54 +0200

    starting the booking system

---
+++ application/views/frontend.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to Booker</title>
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome to Booker!</h1>
+	<a href="index.php/admin">admin</a>
+
+</div>
+
+</body>
+</html>
