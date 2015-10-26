0c6b48110a4b8d0d3720b424c6122fa5510ed493
shpetim <draodakum@gmail.com>
2015-10-26 17:57:43 +0100

    added button for quick booking in the dashboard

--- application/views/backend/dashboard/dashboard.php
+++ application/views/backend/dashboard/dashboard.php
@@ -58,18 +58,27 @@
                       <th>From</th>
                       <th>To</th>
                       <th>Time</th>
+                      <th width="15%" class="text-center">Quick booking</th>
                      </tr>
                   </thead>
                   <tbody>
                     <?php $this->load->model('dashboard_stats'); ?>
 
                     <?php  foreach($upcoming_tours as $dashboard_upcoming): ?>
+                      <?php 
+                      //short variables
+                      $from_name = $this->dashboard_stats->get_city_name($dashboard_upcoming->from);
+                      $to_name = $this->dashboard_stats->get_city_name($dashboard_upcoming->to);
+                      ?>
                       <tr>
                         <td><?php echo $this->dashboard_stats->get_city_name($dashboard_upcoming->from) ?></td>
                         <td><?php echo $this->dashboard_stats->get_city_name($dashboard_upcoming->to) ?></td>
                         <td>
                             <span class="icon-calendar" style="color:red;"></span> <?php echo date('d/m/Y', strtotime($dashboard_upcoming->from_start_time)) ?>
                             <div class="pull-right"><span class="icon-clock" style="color:red;"></span> <?php echo date('H:i', strtotime($dashboard_upcoming->from_start_time)) ?></div>
+                        </td>
+                        <td class="text-center">
+                          <a href="<?php echo base_url().'admin/bookings/process_ticket?from='.$from_name.'&to='.$to_name.'&tickets=1&tour_id='.$dashboard_upcoming->tour_id ?>" class="btn btn-success btn-xs">Book ticket</a>
                         </td>
                       </tr> 
                     <?php  endforeach; ?>
