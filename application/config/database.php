2d0d89cbecba90113184d100b0e7f5be9fa8070d
toocool <draodakum@gmail.com>
2015-12-06 00:07:10 +0100

    test

--- application/config/database.php
+++ application/config/database.php
@@ -44,24 +44,26 @@ if($_SERVER['REMOTE_ADDR'] == "tripexpress-shpetim" ){
     $db['default']['database'] = "tripexpress";
     $db['default']['dbdriver'] = 'mysql';
     $db['default']['dbprefix'] = '';
+}else{
+    $db['default']['hostname'] = "localhost";
+    $db['default']['username'] = "root";
+    $db['default']['password'] = "";
+    $db['default']['database'] = "tripexpress";
+    $db['default']['dbdriver'] = 'mysql';
+    $db['default']['dbprefix'] = '';
 }
-$db['default']['hostname'] = "localhost";
-$db['default']['username'] = "root";
-$db['default']['password'] = "";
-$db['default']['database'] = "tripexpress";
-$db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
+
 
 // The following values can probably stay the same.
  $db['default']['pconnect'] = TRUE;
  $db['default']['db_debug'] = TRUE;
  $db['default']['cache_on'] = FALSE;
--$db['default']['cachedir'] = '';
--$db['default']['char_set'] = 'utf8';
--$db['default']['dbcollat'] = 'utf8_general_ci';
--$db['default']['swap_pre'] = '';
--$db['default']['autoinit'] = TRUE;
--$db['default']['stricton'] = FALSE;
+ $db['default']['cachedir'] = '';
+ $db['default']['char_set'] = 'utf8';
+ $db['default']['dbcollat'] = 'utf8_general_ci';
+ $db['default']['swap_pre'] = '';
+ $db['default']['autoinit'] = TRUE;
+ $db['default']['stricton'] = FALSE;
 
 
 /* End of file database.php */
